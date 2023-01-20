a6e53b8d3ee9f29cdbe096fd41b9a4973684b9d2
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-01-21 02:10:44 +0800

    remove comment in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,3 @@ aqua remote deploy_service \
 --sk $2 \
 --config-path configs/deploy-cfg.json \
 --service ipfs_dag
-
-# aqua remote deploy_service \
-# --addr /ip4/18.134.158.149/tcp/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK \
-# --config-path ../configs/deploy-cfg.json \
-# --service fdb \
-# --sk $2
