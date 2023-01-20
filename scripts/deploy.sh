777e515c3d043baed3a2319d6c477b325bd2177f
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-01-21 02:05:05 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 aqua remote deploy_service \
---addr /dns4/fluence.xfero.io/tcp/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK \
---sk tzxpXKSEweA73XCBUfPybFTYYDrE+HZ5nXBPtub5Eps= \
+--addr $1 \
+--sk $2 \
 --config-path configs/deploy-cfg.json \
---service fdb
+--service ipfs_dag
 
 # aqua remote deploy_service \
 # --addr /ip4/18.134.158.149/tcp/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK \
